f58b80dc174e8796aaa39acacb124c049b3b439f
Joshua <jykim92@gmail.com>
2020-07-07 16:06:13 -0400

    backend communicates with frontend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
-import Home from './components/Home';
+import Login from './components/Login';
 import BudgetContainer from './components/BudgetContainer';
+import Profile from './components/Profile'
 
 
 class App extends React.Component {
 
 
-  renderHome = (routerProps) => {
-    return <Home />
+  renderLogin = (routerProps) => {
+    return <Login />
   }
 
   renderBudgetContainer = (routerProps) => {
     return <BudgetContainer />
+  }
+
+  renderProfile = (routerProps) => {
+    return <Profile />
   }
 
 
@@ -22,7 +27,8 @@ class App extends React.Component {
       <Router>
         <div className="app">
           <NavBar />
-          <Route exact path="/" render={this.renderHome} />
+          <Route exact path="/" render={this.renderLogin} />
+          <Route exact path="/profile" render={this.renderProfile} />
           <Route exact path="/budget" render={this.renderBudgetContainer} />
         </div>
       </Router>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,8 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar">
-      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/">Login</NavLink></li>
+      <li><NavLink to="/profile">Profile</NavLink></li>
       <li><NavLink to="/budget">Budget Tracker</NavLink></li>
     </div>
   );
